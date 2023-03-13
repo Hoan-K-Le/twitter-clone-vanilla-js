f522744c0a2eb98819b0e525692665c64d53d0c4
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-03-13 15:15:44 -0700

    added toggle to hide/unhide the comments

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ tweetBtn.addEventListener('click', function() {
 document.addEventListener('click', function(e) {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like)
-  }
-
-  if (e.target.dataset.retweet) {
+  } else if (e.target.dataset.retweet) {
     handleRetweetClick(e.target.dataset.retweet)
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply)
   }
 })
 
@@ -31,22 +31,57 @@ function handleLikeClick(tweetId) {
 }
 
 function handleRetweetClick(tweetId) {
-  const targetRetweetObj = tweetsData.filter(tweet => {
+  const targetTweetObj = tweetsData.filter(function(tweet) {
     return tweet.uuid === tweetId
   })[0]
-  if (targetRetweetObj.isRetweeted) {
-    targetRetweetObj.retweets--
+
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--
   } else {
-    targetRetweetObj.retweets++
+    targetTweetObj.retweets++
   }
-  targetRetweetObj.isRetweeted = !targetRetweetObj.isRetweeted
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
   render()
+}
+
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
 function getFeedHtml() {
   let feedHtml = ``
 
   tweetsData.forEach(function(tweet) {
+    let likeIconClass = ''
+
+    if (tweet.isLiked) {
+      likeIconClass = 'liked'
+    }
+
+    let retweetIconClass = ''
+
+    if (tweet.isRetweeted) {
+      retweetIconClass = 'retweeted'
+    }
+
+    let repliesHtml = ''
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function(reply) {
+        repliesHtml += `
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+      })
+    }
+
     feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -62,13 +97,13 @@ function getFeedHtml() {
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-heart"
+                    <i class="fa-solid fa-heart ${likeIconClass}"
                     data-like="${tweet.uuid}"
                     ></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"
+                    <i class="fa-solid fa-retweet ${retweetIconClass}"
                     data-retweet="${tweet.uuid}"
                     ></i>
                     ${tweet.retweets}
@@ -76,6 +111,9 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
 </div>
 `
   })
