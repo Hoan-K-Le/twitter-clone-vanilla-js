4176c7773e2a24140b137b2771217b9ec586238b
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2023-03-13 15:39:51 -0700

    added functionality to tweet

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import { tweetsData } from './data.js'
-const tweetInput = document.getElementById('tweet-input')
-const tweetBtn = document.getElementById('tweet-btn')
-
-tweetBtn.addEventListener('click', function() {
-  console.log(tweetInput.value)
-})
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 
 document.addEventListener('click', function(e) {
   if (e.target.dataset.like) {
@@ -13,6 +8,8 @@ document.addEventListener('click', function(e) {
     handleRetweetClick(e.target.dataset.retweet)
   } else if (e.target.dataset.reply) {
     handleReplyClick(e.target.dataset.reply)
+  } else if (e.target.id === 'tweet-btn') {
+    handleTweetBtnClick()
   }
 })
 
@@ -46,6 +43,26 @@ function handleRetweetClick(tweetId) {
 
 function handleReplyClick(replyId) {
   document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+}
+
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById('tweet-input')
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    })
+    render()
+    tweetInput.value = ''
+  }
 }
 
 function getFeedHtml() {
